3f57a21841006881576914d86209dff2b6bca125
Antony Nyarangi <antony.nyarangi@interswitchgroup.com>
2020-04-26 22:04:27 +0300

    feature extraction

--- api.py
+++ api.py
@@ -48,4 +48,58 @@ def vectorize(xtrain,xtest):
     test_vectors = vectorizer.transform(xtest)
     print(train_vectors.shape, test_vectors.shape)
 
-# classifier
+# calculating frequency of stop words
+def stopwords_frequency(text):
+    from nltk.corpus import stopwords
+    english_stop_words = stopwords.words('english')
+    stopword_count = 0
+    for word in text.split():
+        if word in english_stop_words:
+            stopword_count = stopword_count + 1
+    return stopword_count
+
+# calculating the frequency ratio of stopwords vs text length
+def stopwords_frequency_ratio(text):
+    stopwords_count = stopwords_frequency(text)
+    text_length = len(text.split())
+    frequency_ratio = stopwords_count/float(text_length)
+    return frequency_ratio
+
+def sentence_length_distribution(text):
+    # breakdown text into sentences
+    sentences = nltk.tokenize.sent_tokenize(text)
+    # determine the distribution of sentences
+        # simple sentence - has no conjunctions
+        # compound sentence = has one conjunction joining two sentences
+        # complex sentence = has one or more conjunctions
+        # conjunctions can be either coordinating/subordinating
+        # coordinating_conjunctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+        # subordinating_conjunctions = ["after", "as", "although", "because", "before", "even though", "if", "once", "rather than", "since", "that", "though", "unless", "until", "when", "whenever", "whereas", "while"]
+    conjunctions = ["for", "and", "nor", "but", "or", "yet", "so","after", "as", "although", "because", "before", "even though", "if", "once", "rather than", "since", "that", "though", "unless", "until", "when", "whenever", "whereas", "while"]
+    # number of conjunctions in sentences
+    conjunction_distribution = []
+    for sentence in sentences:
+        sentence_conjunction_count = 0
+        for word in sentence.split():
+            if word in conjunctions:
+                sentence_conjunction_count += 1
+        conjunction_distribution.append(sentence_conjunction_count)
+
+    simple_sentence_count = 0
+    compound_sentence_count = 0
+    complex_sentence_count = 0
+
+    # categorizing sentences based on conjunction count
+    for count in conjunction_distribution:
+        if count == 0:
+            simple_sentence_count += 1
+        elif count == 1:
+            compound_sentence_count += 1
+        elif count > 1:
+            complex_sentence_count += 1
+
+    simple_sentence_distribution = simple_sentence_count/float(len(sentences))
+    compound_sentence_distribution = compound_sentence_count/float(len(sentences))      
+    complex_sentence_distribution = complex_sentence_count/float(len(sentences))      
+
+    return [simple_sentence_distribution, compound_sentence_distribution, complex_sentence_distribution]
